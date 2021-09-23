25d30a1339e08c5d92e8249e50e51aaa608a816c
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-09-23 12:04:30 +0530

    main

---
+++ Practicals/bubble_sort.py
@@ -0,0 +1,18 @@
+i_arr=[7,6,10,5,9,2,213,1,15,10,25,7]#Bubble Sort
+
+#Bubble Sort Function
+def bubbleSort(arr):
+    for j in range(len(arr)):
+        for i in range(len(arr)-1):
+            num=arr[i]
+            next_num=arr[i+1]
+            if num>next_num:
+                arr[i]=next_num
+                arr[i+1]=num
+
+#Driver Code
+if True:
+    arr=i_arr.copy()
+    print(arr)
+    bubbleSort(arr)
+    print(arr)
