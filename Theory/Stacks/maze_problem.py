b97a1f00bc4e50afe52f87011e958c91186a44ba
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-09-24 02:56:23 +0530

    main

--- Theory/Stacks/maze_problem.py
+++ Theory/Stacks/maze_problem.py
@@ -1,23 +1,23 @@
 #Function to create a Maze
 def Maze(r,c):
     maze=list()
-    for _ in range(r):
-        col=list()
-        for _2 in range(c):
-            col.append(_+(r*_2))
-        maze.append(col)
+    for _ in range(c):
+        row=list()
+        for _2 in range(r):
+            row.append(_2+(c*_))
+        maze.append(row)
     return maze
 
 #Function to create walls in the maze
 def setwall(lst, maze):
     for i in range(len(maze)):
         for j in range(len(maze[i])):
-            if maze[i][j]=='S' or maze[i][j]=='E':
+            if maze[j][i]=='S' or maze[j][i]=='E':
                 continue
-            elif maze[i][j] not in lst:
-                maze[i][j]=1
+            elif maze[j][i] not in lst:
+                maze[j][i]=1
             else:
-                maze[i][j]=0
+                maze[j][i]=0
     return maze
 
 #Function to create the start point
@@ -25,7 +25,7 @@ def setstart(start,maze):
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             if i+(len(maze)*j)==start:
-                maze[i][j]="S"
+                maze[j][i]="S"
     return maze
 
 #Function to create the end point
@@ -33,11 +33,14 @@ def setend(end,maze):
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             if i+(len(maze)*j)==end:
-                maze[i][j]="E"
+                maze[j][i]="E"
     return maze
 
 #Function to reset the Maze to the initial state
-def reset(lst,maze):
+def reset(start,end,lst,maze):
+    maze=Maze(len(maze),len(maze[0]))
+    maze=setstart(start,maze)
+    maze=setend(end,maze)
     maze=setwall(lst,maze)
     return maze
 
@@ -45,53 +48,64 @@ def reset(lst,maze):
 def findpath(prev,curr,path,maze): 
     return path
 
+#Function to print Maze
+def print_maze(maze):
+    for _ in maze:
+        print(_)
 
 #Driver Code
+if True:
+    #Genrate a maze
+    r_n_c=input('Enter the number of rows and columns: (Format: rows,columns)\n')
+    [r,c]=r_n_c.split(',')
+    r,c=int(r),int(c)
+    maze=Maze(r,c)
+    print_maze(maze)
 
-#Genrate a maze
-r_n_c=input('Enter the number of rows and columns: (Format: rows,columns)\n')
-[r,c]=r_n_c.split(',')
-r,c=int(r),int(c)
-maze=Maze(r,c)
+    #Taking input of start and end
+    start=input(f'Enter the Start index in range 0 to {r*c-1}:\n')
+    end=input(f'Enter the End index in range 0 to {r*c-1} except '+start+':\n')
+    boundries=list()
+    for i in range(len(maze)):
+        if i==0 or i==c-1:
+            j=0
+            while j<r:
+                boundries.append(i+c*j)
+                j+=1
+        else:
+            boundries.append(i+0)
+            boundries.append(i+(r-1)*c)
+    #print(boundries)
 
-
-#Setting Starting and Ending points
-start=input(f'Enter the Start index in range 0 to {r*c-1}:\n')
-end=input(f'Enter the End index in range 0 to {r*c-1} except '+start+':\n')
-boundries=list()
-for i in range(len(maze)):
-    if i==0 or i==r-1:
-        j=0
-        while j<c:
-            boundries.append(i+r*j)
-            j+=1
+    if start==end and start not in boundries and end not in boundries:
+        print('Start and End cannot be placed at the given indices!!')
     else:
-        boundries.append(i+0)
-        boundries.append(i+(c-1)*r)
-#print(boundries)
+        #Setting Starting and Ending points
+        start=int(start)
+        end=int(end)
+        maze=setstart(start,maze)
+        maze=setend(end,maze)
 
-if start==end and start not in boundries and end not in boundries:
-    print('Start and End cannot be placed at the given indices!!')
-else:
-    start=int(start)
-    end=int(end)
-    maze=setstart(start,maze)
-    maze=setend(end,maze)
+        #Put Walls
+        walls=input(f'Enter the wall indices in range 0 to {r*c-1} except {start,end}: (Format 0,1,2,...)\n')
+        wall_list=walls.split(',')
+        #print(wall_list)
+        for i in range(len(wall_list)):
+            wall_list[i]=int(wall_list[i])
+        maze=setwall(wall_list, maze)
+        print_maze(maze)
 
-    #Put Walls
-    walls=input(f'Enter the wall indices in range 0 to {r*c-1} except {start,end}: (Format 0,1,2,...)\n')
-    wall_list=walls.split(',')
-    #print(wall_list)
-    for i in range(len(wall_list)):
-        wall_list[i]=int(wall_list[i])
-    maze=setwall(wall_list, maze)
-    print(maze)
+        #Find Path
+        #path=[]
+        #path=findpath((si,sj),(ei,ej),path,maze)
 
-    #path=[]
-    #path=findpath((si,sj),(ei,ej),path,maze)
+        #Reset Maze
+        #maze=reset(start,end,wall_list,maze)
+        #print_maze(maze)
+    
     
 #Maze 1
     #Maze Shape: 7x7
-    #Walls: 0,1,2,3,4,5,6,7,13,14,15,16,18,21,23,25,27,28,32,33,34,35,37,40,41,42,44,45,46,47,48
-    #Start: 42
+    #Walls: 0,1,2,3,4,5,6,7,13,14,15,16,18,21,23,25,27,28,32,33,34,35,37,41,42,44,45,46,47,48
+    #Start: 43
     #End: 20
