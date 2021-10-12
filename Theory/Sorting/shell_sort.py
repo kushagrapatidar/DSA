8724f129113bcef47c5f8b04b06fdefe9c7c2b79
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-12 16:53:38 +0530

    main

--- Theory/Sorting/shell_sort.py
+++ Theory/Sorting/shell_sort.py
@@ -96,12 +96,3 @@ def shellsort(arr):
             #print(i,":",arr)
         #print("\n")
     return arr
-
-#Driver Code
-'''if True:
-    arr=[3,7,1,8,2,5,9,4,6]
-    #arr=[23,29,15,19,31,7,9,5,2]
-    print(f"Initial Array:\n{arr}\n")
-    
-    arr=shellsort(arr)
-    print(f"Sorted Array:\n{arr}")'''
