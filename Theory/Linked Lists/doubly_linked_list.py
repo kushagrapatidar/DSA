18dec5ced8151b5ee6a14245a55e7cafe73c83bd
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-17 02:56:51 +0530

    main

--- Theory/Linked Lists/doubly_linked_list.py
+++ Theory/Linked Lists/doubly_linked_list.py
@@ -263,6 +263,7 @@ def operate(head,tail):
     ch=input("Do you want to conduct more operations??('Y' for yes & 'N' for no): ")
     if ch=='Y' or ch=='y':
         operate(head,tail)
+
 #Tail Traverse
 #if True:
     #data=""
