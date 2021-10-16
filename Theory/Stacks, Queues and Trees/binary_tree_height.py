ef5e23139bf0390d3bd5050028bb982e56d75b43
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-16 13:11:36 +0530

    main

--- Theory/Stacks, Queues and Trees/binary_tree_height.py
+++ Theory/Stacks, Queues and Trees/binary_tree_height.py
@@ -4,6 +4,6 @@ def find_height(arr,height,i=0):
         i=2*i+1
         height=find_height(arr,height,i)
     return height
-arr=[2,3,9,4,5]
+arr=[2,3,9,4,5,2]
 height=find_height(arr,0)
 print(f'Height of the tree {arr} is {height}')
