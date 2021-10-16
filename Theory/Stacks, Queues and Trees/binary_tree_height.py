325dddf4a1176a55b8b3220695d2359ea19dc749
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-16 13:08:49 +0530

    main

---
+++ Theory/Stacks, Queues and Trees/binary_tree_height.py
@@ -0,0 +1,8 @@
+def find_height(arr,height,i=0):
+    if 2*i+1<len(arr):
+        height+=1
+        i=2*i+1
+        height=find_height(arr,height,i=0)
+    return height
+arr=[2,3,9,4,5]
+height=find_height(arr,0)
