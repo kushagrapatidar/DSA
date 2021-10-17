f538a1537f0bc7cb1301f18e7355b43e409d657c
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-18 00:08:04 +0530

    main

--- Theory/Linked Lists/test.py
+++ Theory/Linked Lists/test.py
@@ -1,3 +1,28 @@
+#Driver Code: Singly Linked List
+'''if True:
+    from singly_linked_list import operate
+    head=tail=None
+    operate(head,tail)
+    #Test List
+        #Ins - 'Lists'
+        #Ins - beg - 'Linked'
+        #Ins - end - 'is'
+        #Ins - end - 'a'
+        #Ins - 1 - 'Doubly'
+        #Ins - 7 - 'Structure'
+        #Ins - 6 - 'Data'
+        #Trav
+        #Srch - 'a'
+        #Updt - 'Lists' to 'List'
+        #Trav
+        #Del - 5
+        #Del - beg
+        #Del - end
+        #Trav
+        #Rev
+        #Trav
+    #'''
+
 #Driver Code: Doubly Linked List
 '''if True:
     from doubly_linked_list import operate
@@ -18,4 +43,32 @@
         #Del - 5
         #Del - beg
         #Del - end
+        #Trav
+        #Rev
+        #Trav
+    #'''
+
+#Driver Code: Circular Linked List
+'''if True:
+    from circular_linked_list import operate
+    head=tail=None
+    operate(head,tail)
+    #Test List
+        #Ins - 'Lists'
+        #Ins - beg - 'Linked'
+        #Ins - end - 'is'
+        #Ins - end - 'a'
+        #Ins - 1 - 'Doubly'
+        #Ins - 7 - 'Structure'
+        #Ins - 6 - 'Data'
+        #Trav
+        #Srch - 'a'
+        #Updt - 'Lists' to 'List'
+        #Trav
+        #Del - 5
+        #Del - beg
+        #Del - end
+        #Trav
+        #Rev
+        #Trav
     #'''
