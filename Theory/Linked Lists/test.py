2f74708f1a67b6522e614834e6f288291e9facf8
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-21 14:21:30 +0530

    main

--- Theory/Linked Lists/test.py
+++ Theory/Linked Lists/test.py
@@ -74,10 +74,12 @@
     #'''
 
 #Driver Code: Binary Tree Height
-'''if True:
+if True:
     from tree import make_tree
     from binary_tree_height import find_height_ll
     tree_head=make_tree()
     find_height_ll(tree_head)
+    #arr=[2,3,9,4]
+    #max_height = 2, min_height=1
     #arr=[2,3,9,4,5,10,11,6] 
     #max_height = 3, min_height = 2 '''
