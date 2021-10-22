347fd2714fc2ba255d869baaa72575772fda43c1
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-22 17:21:33 +0530

    main

--- Theory/Linked Lists/heapyfy.py
+++ Theory/Linked Lists/heapyfy.py
@@ -14,12 +14,13 @@ def heapyfy(tree_root):
             tree_root.right,tree_root.left=right.right,right.left
             right.right,right.left=tree_root,left
             tree_root=right
+            
     elif right!=None:
         if left.data>tree_root.data:
             tree_root.right,tree_root.left=left.right,left.left
-            left.left,left.right=tree_root,left
+            left.left,left.right=tree_root,right
             tree_root=left
-    
+
     elif max(tree_root.data,max(right.data,left.data))!=tree_root.data:
         if max(right.data,left.data)==right.data:
             tree_root.right,tree_root.left=right.right,right.left
@@ -27,7 +28,7 @@ def heapyfy(tree_root):
             tree_root=right
         else:
             tree_root.right,tree_root.left=left.right,left.left
-            left.left,left.right=tree_root,left
+            left.left,left.right=tree_root,right
             tree_root=left
 
     return tree_root
