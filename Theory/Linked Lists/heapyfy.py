09c7c2fcd7a8dd31a6de56e0615cffb7d582a18f
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-22 17:20:02 +0530

    main

--- Theory/Linked Lists/heapyfy.py
+++ Theory/Linked Lists/heapyfy.py
@@ -7,16 +7,27 @@ def heapyfy(tree_root):
         right=heapyfy(right)
     if left!=None:
         left=heapyfy(left)
-    try:
-        if max(tree_root.data,max(right.data,left.data))!=tree_root.data:
-            if max(right.data,left.data)==right.data:
-                tree_root.right,tree_root.left=right.right,right.left
-                right.right,right.left=tree_root,left
-                tree_root=right
-            else:
-                tree_root.right,tree_root.left=left.right,left.left
-                left.left,left.right=tree_root,left
-                tree_root=left
-    except AttributeError:    
+    if right==None and left==None:
         return tree_root
+    elif left==None:
+        if right.data>tree_root.data:
+            tree_root.right,tree_root.left=right.right,right.left
+            right.right,right.left=tree_root,left
+            tree_root=right
+    elif right!=None:
+        if left.data>tree_root.data:
+            tree_root.right,tree_root.left=left.right,left.left
+            left.left,left.right=tree_root,left
+            tree_root=left
+    
+    elif max(tree_root.data,max(right.data,left.data))!=tree_root.data:
+        if max(right.data,left.data)==right.data:
+            tree_root.right,tree_root.left=right.right,right.left
+            right.right,right.left=tree_root,left
+            tree_root=right
+        else:
+            tree_root.right,tree_root.left=left.right,left.left
+            left.left,left.right=tree_root,left
+            tree_root=left
+
     return tree_root
