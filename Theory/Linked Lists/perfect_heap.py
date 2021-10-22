75f2b830bf3b86132de68baf09ee4e4ade51ad9d
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-23 00:14:58 +0530

    main

--- Theory/Linked Lists/perfect_heap.py
+++ Theory/Linked Lists/perfect_heap.py
@@ -1,12 +1,16 @@
 #Function to check the validity of the given heap as perfect or not
 def check_heap(tree_root):
-    if tree_root==None:
-        return False
+    left=tree_root.left
+    right=tree_root.right
+
+    if left.left!=None and left.right!=None and right.left!=None and right.right!=None:
+        if tree_root==None:
+            return False
     
-    if tree_root.left==None and tree_root.right==None:
-        return True
+        if left==None and right==None:
+            return True
     
-    if tree_root.left!=None and tree_root.right!=None:
-        return (check_heap(tree_root.left) and check_heap(tree_root.right))
+        if left!=None and right!=None:
+            return (check_heap(left) and check_heap(right))
     
     return False
