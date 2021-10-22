98ec1c6a04caf7a523c7092cb31b86d811b48073
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-22 17:22:50 +0530

    main

--- Theory/Linked Lists/heapyfy.py
+++ Theory/Linked Lists/heapyfy.py
@@ -7,14 +7,16 @@ def heapyfy(tree_root):
         right=heapyfy(right)
     if left!=None:
         left=heapyfy(left)
+    
     if right==None and left==None:
         return tree_root
+    
     elif left==None:
         if right.data>tree_root.data:
             tree_root.right,tree_root.left=right.right,right.left
             right.right,right.left=tree_root,left
             tree_root=right
-            
+
     elif right!=None:
         if left.data>tree_root.data:
             tree_root.right,tree_root.left=left.right,left.left
