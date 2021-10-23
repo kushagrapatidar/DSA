c1abffd57cfb7311e572db084ac5230d897229c4
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-23 12:34:10 +0530

    main

--- Theory/Linked Lists/check_heap.py
+++ Theory/Linked Lists/check_heap.py
@@ -1,9 +1,23 @@
+#Function to check if a tree is perfect or not
 def isperfect(tree_root):
     from binary_tree_height import find_height_ll
     max_height,min_height=find_height_ll(tree_root)
     
     val=(max_height==min_height)
     if val:
-        print("Perfect")
+        print("Perfect Tree")
     else:
-        print("Not Perfect")
+        print("Not Perfect Tree")
+
+#Function to check if the tree is full or not
+def isfull(tree_root):
+    if tree_root==None:
+        return False
+    
+    if tree_root.left==None and tree_root.right==None:
+        return True
+    
+    if tree_root.left!=None and tree_root.right!=None:
+        return (isfull(tree_root.left) and isfull(tree_root.right))
+    
+    return False
