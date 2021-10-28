d06e14f0ca434c397aaa39d7e82fe1792fe4f11c
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-28 11:39:55 +0530

    main

--- Practicals/Experiment_06.py
+++ Practicals/Experiment_06.py
@@ -5,7 +5,7 @@ def floyd_warshall(graph):
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                graph[i][j]=min(graph[i][j],graph[i][k]+A[k][j])
+                graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
 
     return graph
 
