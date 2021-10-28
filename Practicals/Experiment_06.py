f3599528fbc3ba0077ee3d35e3ec30459159b236
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-28 11:56:31 +0530

    main

--- Practicals/Experiment_06.py
+++ Practicals/Experiment_06.py
@@ -22,12 +22,14 @@ def print_graph(graph):
 #Driver Code
 if True:
     INF=float('INF')
+    #A0 passing
     graph= [[0,3,INF,7],
         [8,0,2,INF],
         [5,INF,0,1],
         [2,INF,INF,0]]
-
+    # print(graph[0][2])
     print_graph(graph)
+    print("\n")
     # print(f"\n{len(graph)}\n")
 
     shortest_path=floyd_warshall(graph)
