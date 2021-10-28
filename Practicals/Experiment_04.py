30ed28f7d21b6ee02147da330707fc5e633e97ae
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-29 01:26:16 +0530

    main

--- Practicals/Experiment_04.py
+++ Practicals/Experiment_04.py
@@ -1,1 +1,40 @@
 #N-Queens problem
+def check_rows(board,N,c):
+    for i in range(N):
+        if board[i][c]!=1:
+            return True
+    return False
+
+def check_colms(board,N,r):
+    for j in range(N):
+        if board[r][j]!=1:
+            return True
+    return False
+
+def soln_nqueens(board,N):
+    for i in range(N):
+        for j in range(N):
+            if check_rows(board,N,j) and check_colms(board,N,i):
+                board[i][j]=1
+    return board
+
+def print_board(board,N):
+    print('\n')
+    for i in range(N):
+        for j in range(N):
+            print(board[i][j],end="  ")
+        print('\n')
+
+#Driver Code
+if True:
+    N=int(input('Enter the number of Queens: '))
+    board=[]
+    for i in range(N):
+        lst=list()
+        for j in range(N):
+            lst.append(0)
+        board.append(lst)
+    
+    # print_board(board,N)
+    board=soln_nqueens(board,N)
+    print_board(board,N)
---
+++ Practicals/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+board=soln_nqueens(board,N)
+    print_board(board,N)
