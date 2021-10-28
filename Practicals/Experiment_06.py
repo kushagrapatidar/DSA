8f755329843ad8eba12df769a52ee6b2f1d5b7e0
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-28 11:36:43 +0530

    main

--- Practicals/Experiment_06.py
+++ Practicals/Experiment_06.py
@@ -1,18 +1,32 @@
 #Ak[i,j]=min{A[i,j],A[i,k]+A[k,j]}
-def floyd_warshall(A):
-    n=len(A)
+def floyd_warshall(graph):
+    n=len(graph)
+    k=1
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                A[i][j]=min(A[i][j],A[i][k]+A[k][j])
+                graph[i][j]=min(graph[i][j],graph[i][k]+A[k][j])
 
-    return A
+    return graph
+
+def print_graph(graph):
+    n=len(graph)
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j]==float('inf'):
+                print('INF',end=" ")
+            else:
+                print(A[i][j],end=" ")
+        print("\n")
+
 INF=float('INF')
-A= [[0,5,INF,10],
+graph= [[0,5,INF,10],
     [INF,0,3,INF],
     [INF,INF,0,1],
     [INF,INF,INF,0]]
-    
-print(A,"\n")
-A=floyd_warshall(A)
-print(A)
+
+print_graph(graph)
+print(f"\n{len(A)}\n")
+
+graph=floyd_warshall(graph)
+print_graph(graph)
