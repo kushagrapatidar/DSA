b53d178eb216d9f720d7281bcfc9b999af6eb951
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-28 11:26:56 +0530

    main

---
+++ Practicals/Experiment_06.py
@@ -0,0 +1,17 @@
+#Ak[i,j]=min{A[i,j],A[i,k]+A[k,j]}
+def floyd_warshall(A):
+    n=len(A)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                A[i][j]=min(A[i][j],A[i][k]+A[k][j])
+
+    return A
+INF=float('INF')
+A= {{0,5,INF,10},
+    {INF,0,3,INF},
+    {INF,INF,0,1},
+    {INF,INF,INF,0}}
+print(A,"\n")
+A=floyd_warshall(A)
+print(A)
