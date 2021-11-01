acbe9d4c7b11d8ba3e71863d7393807cac62bc2a
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-11-02 01:18:15 +0530

    main

--- Practicals/Experiment_04.py
+++ Practicals/Experiment_04.py
@@ -1,60 +1,31 @@
 #N-Queens problem
-# def check_rows(board,N,c):
-#     for i in range(N):
-#         if board[i][c]==1:
-#             return False
-#     return True
-
-# def check_colms(board,N,r):
-#     for j in range(N):
-#         if board[r][j]==1:
-#             return False
-#     return True
-
-# def check_diag(board,N,i,j):
-#     if 0<i<N-1 and 0<j<N-1 and (board[i-1][j-1]==1 or board[i+1][j+1]==1 or board[i-1][j+1]==1 or board[i+1][j-1]==1):
-#         return False
-#     elif (i==0 and 0<j<N and board[i-1][j+1]==1) or (i==N and 0<j<N and board[i-1][j-1]==1):
-#         return False
-#         #CONTINUE HERE
-        
-#     return True
-
-# def soln_nqueens(board,N):
-#     board[0][1]=1
-#     for i in range(N):
-#         for j in range(N):
-#             if check_rows(board,N,j) and check_colms(board,N,i) and check_diag(board,N,i,j):
-#                 board[i][j]=1
-#     return board
-
 def set_row_colm(board,i,j):
     for _ in range(len(board)):
         if board[i][_]!=1:
             board[i][_]='O'
+    for _ in range(len(board)):
         if board[_][j]!=1:
             board[_][j]='O'
     return board
 
 def set_diag(board,i,j):
-
+    #For diagonally upward on left
     tempi,tempj=i,j
-    print(tempi,tempj)
     while tempi>=0 and tempj>=0:
         if board[tempi][tempj]!=1:
             board[tempi][tempj]='O'
         tempi-=1
         tempj-=1
     
-    print(tempi,tempj)
+    #For diagonally downward on right
     tempi,tempj=i,j
-    print(tempi,tempj)
     while 0<=tempi<N and 0<=tempj<N:
         if board[tempi][tempj]!=1:
             board[tempi][tempj]='O'
         tempi+=1
         tempj+=1
 
+    #For diagonally downward on left
     tempi,tempj=i,j
     while 0<=tempi<N and tempj>=0:
         if board[tempi][tempj]!=1:
@@ -62,6 +33,7 @@ def set_diag(board,i,j):
         tempi+=1
         tempj-=1
 
+    #For diagonally upward on right
     tempi,tempj=i,j
     while tempi>=0 and 0<=tempj<N:
         if board[tempi][tempj]!=1:
@@ -73,7 +45,12 @@ def set_diag(board,i,j):
 
 def soln_nqueens(board,N):
     if N>0:
-        board[0][1]=1
+        if N==len(board):
+            board[0][1]=1
+            board=set_row_colm(board,0,1)
+            print_board(board,len(board))
+            board=set_diag(board,0,1)
+        
         for i in range(len(board)):
             if 1 not in board[i]:
                 for j in range(len(board)):
--- Practicals/tempCodeRunnerFile.py
+++ Practicals/tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-for i in range(N):
+    # for i in range(N):
     #     for j in range(N):
     #         if board[i][j]=='O':
     #             board[i][j]=0
