8cb52aae16ed73824fb3ddbf4261a3776f0d9bc0
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-11-06 01:25:35 +0530

    main

--- Theory/Linked Lists/test.py
+++ Theory/Linked Lists/test.py
@@ -74,35 +74,35 @@
     #'''
 
 #Driver Code: Tree Operations
-# if True:
-#     from tree import make_tree,print_tree
-#     from binary_tree_height import find_height_ll
-#     from heapyfy import heapyfy
-#     import check_heap as CH
+#if True:
+    # from tree import make_tree,print_tree
+    # from binary_tree_height import find_height_ll
+    # from heapyfy import heapyfy
+    # import check_heap as CH
 
-#     tree_root=make_tree()
+    # tree_root=make_tree()
     
-#     #Heapyfy Operation
-#     tree_root=heapyfy(tree_root)
+    # #Heapyfy Operation
+    # tree_root=heapyfy(tree_root)
     
-#     #Heights of a Binary Tree Operation
-#     max_height,min_height=find_height_ll(tree_root)
+    # #Heights of a Binary Tree Operation
+    # max_height,min_height=find_height_ll(tree_root)
 
-#     if max_height==-1:
-#         print("There are no Nodes")     
-#     else:
-#         print(f"Maximum and Minimum heights of the tree are {max_height,min_height}")
+    # if max_height==-1:
+    #     print("There are no Nodes")     
+    # else:
+    #     print(f"Maximum and Minimum heights of the tree are {max_height,min_height}")
     
-#     #Full and Perfect Binary Trees
-#     CH.isfull(tree_root)
-#     CH.isperfect(tree_root)
+    # #Full and Perfect Binary Trees
+    # CH.isfull(tree_root)
+    # CH.isperfect(tree_root)
 
-#     print_tree(tree_root)
-#     #Test Cases:
-#         #arr=[2,3,9,4]
-#         #max_height = 2, min_height=1
-#         #arr=[2,3,9,4,5,10,11,6]
-#         #max_height = 3, min_height = 2
+    # print_tree(tree_root)
+    # #Test Cases:
+    #     #arr=[2,3,9,4]
+    #     #max_height = 2, min_height=1
+    #     #arr=[2,3,9,4,5,10,11,6]
+    #     #max_height = 3, min_height = 2
 
 #Tree printing algo Dev.
 # if True:
