559609ebec27698ad588329c857b1a1e1c6aca24
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-11-11 23:15:43 +0530

    main

--- Practicals/Experiment_07.py
+++ Practicals/Experiment_07.py
@@ -1,10 +1,10 @@
 def print_graph(graph):
     n=len(graph)
     print('   ',end='')
-    [print(f'\033[1;32;40m{x}',end="  ") for x in range(1,n+1)]
+    [print(f'\033[1;33;40m{x}',end="  ") for x in range(1,n+1)]
     print('\n')
     for i in range(1,n+1):
-        print(f'\033[1;34;40m{i}\033[1;37;40m',end="  ")
+        print(f'\033[1;31;40m{i}\033[1;37;40m',end="  ")
             
         for j in range(1,n+1):
             print(f'{graph[i-1][j-1]}',end="  ")
